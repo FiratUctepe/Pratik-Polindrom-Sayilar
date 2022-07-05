fb89ce49f0234e5cabb87280f5fc95742289c4b0
FıratUctepe <108686427+FiratUctepe@users.noreply.github.com>
2022-07-05 14:49:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    int number,temp,terssayi=0;
+    Scanner scan=new Scanner (System.in);
+    System.out.println("Lütfen bir sayı giriniz.");
+    number = scan.nextInt();
+    temp=number;
+    while(temp!=0){
+      int sonsayı=temp%10;
+      terssayi=terssayi*10+sonsayı;
+      temp/=10;
+    } System.out.println("Girdiğiniz sayi: "+number);
+
+      System.out.println("Girdiğiniz sayının tersi: "+terssayi);
+    if(number==terssayi){
+      System.out.println(number+" sayısı palindrom bir sayıdır.");
+    }else{
+      System.out.println(number+" sayısı palindrom bir sayı değildir.");
+
+    }
+  }
+}
